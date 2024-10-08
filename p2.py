befad112500e5711e12c48d19aa959f1d1493b40
kkhatoon1 <kkhatoon1@my.nl.edu>
2024-10-08 15:54:51 -0500

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,17 @@
+#khateeja khatoon
+#oct 8 2024
+#this program ask user their name and greets them with their name
+
+name = input("what is your name?")
+print ("hello ," + name + " glad to meet you" + "!")
+
+# this program ask for their name and greet them with their name if authorized
+name = input("what is your name?")
+name1 = "khateeja"
+name2 = "Ben"
+#if it is print hello
+if name == name1: 
+  print ( "hello," + name + "!")
+else:
+  print ("unauthorized user")
+# this program allows only specific users
